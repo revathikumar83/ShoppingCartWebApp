6f72c99c1f7a2c48579c27c205393dcd5922a8f4
revathi <revathikumar83@gmail.com>
2020-12-21 18:50:08 -0500

    server update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
 
 
 
-mongoose.connect( "mongodb://localhost/shopping-cartapp", 
+mongoose.connect( process.env.MONGODB_URL || "mongodb://localhost/shopping-cartapp", 
 {    useNewUrlParser: true,
      useUnifiedTopology: true, 
      useUnifiedTopology:true
